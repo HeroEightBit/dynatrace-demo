eeda98f95b3dd063fb90d05202c76c7c3b912a6e
aaron.schultz <aaron.schultz@dynatrace.com>
2025-10-20 13:09:21 -0400

    fix: Refactor layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-
-// Use a Client Component for username bar state
 "use client";
+import Navbar from "./components/navbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-import Navbar from "./components/navbar";
+import Image from "next/image";
 
 
 const geistSans = Geist({
@@ -18,11 +16,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
-
-
-
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -30,7 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         style={{ margin: 0, padding: 0 }}
       >
-        <div style={{ display: 'flex', flexDirection: 'row', minHeight: '100vh' }}>
+  <div style={{ display: 'flex', flexDirection: 'row', minHeight: '100vh' }}>
           <Navbar />
           <div className="main-content">{children}</div>
         </div>
